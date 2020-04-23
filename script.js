5ebe1c51a2bd86026d9e9bbe0cf716e878828c5e
unknown <dvandenbrink@roc-dev.com>
2020-04-23 14:20:17 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -7,16 +7,24 @@ function submit() {
     inputdiameter = document.getElementById('diameter').value;
     omtrekcirkel = inputdiameter * Math.PI;
     inhoudcircel = inputdiameter ** 2 * Math.PI / 4;
-    document.getElementById('show2').innerHTML = inhoudcircel;
-    document.getElementById("show").innerHTML = omtrekcirkel;
+    document.getElementById('show2').innerHTML = "Inhoud circel :" + inhoudcircel;
+    document.getElementById("show").innerHTML = "Omtrek circel :" + omtrekcirkel;
 }
 
 function time() {
     var date = new Date();
-    var hour = date.getHours();
+    var hours = date.getHours();
     var minutes = date.getMinutes();
-    var second = date.getSeconds();
+    var seconds = date.getSeconds();
     
-    document.getElementById('TimeDate').innerHTML = "Time = " + hour + ":" + minutes + ":" + second;
+    minutes = checkSeconds(minutes);
+    seconds = checkSeconds(seconds);
 
+    document.getElementById('TimeDate').innerHTML = "Time = " + hours + ":" + minutes + ":" + seconds;
+
+    var t = setTimeout(time, 500);
+}
+function checkSeconds(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
 }
